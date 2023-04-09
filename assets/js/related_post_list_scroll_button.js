40519cd7ea0f0c0c9c7be84c338c25acfc49e679
GiGong <gigong222@gmail.com>
2023-04-09 20:56:17 +0900

    Refactoring and design related post list button

--- assets/js/related_post_list_scroll_button.js
+++ assets/js/related_post_list_scroll_button.js
@@ -1,10 +1,10 @@
-document.getElementById("btn-left")
+document.getElementById("btn-related-post-left")
   .addEventListener('click', function (event) {
     targetList = document.getElementById("related-post-list")
     targetList.scrollLeft -= targetList.clientWidth / 2;
   });
 
-document.getElementById("btn-right")
+document.getElementById("btn-related-post-right")
   .addEventListener('click', function (event) {
     targetList = document.getElementById("related-post-list")
     targetList.scrollLeft += targetList.clientWidth / 2;
